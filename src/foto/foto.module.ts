46d527361fd537be6b6e89470ae7057c131f9338
Alejandro Tovar <da.tovar10@uniandes.edu.co>
2023-12-02 17:02:38 -0500

    Minor fix

--- src/foto/foto.module.ts
+++ src/foto/foto.module.ts
@@ -3,9 +3,10 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { FotoEntity } from '../foto/foto.entity';
 import { FotoService } from '../foto/foto.service';
+import { AlbumEntity } from '../album/album.entity';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([FotoEntity])],
+    imports: [TypeOrmModule.forFeature([FotoEntity, AlbumEntity])],
     providers: [FotoService],
     controllers: [],
 })
